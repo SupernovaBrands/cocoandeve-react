d986237988a148fc48427767d465348af7394ec1
rizkiaprilian <rizki.a.aprilian@gmail.com>
2022-03-29 16:17:24 +0700

    update tan range page - banner & formula section

--- src/UI/sections/RangeBanner.js
+++ src/UI/sections/RangeBanner.js
@@ -1,9 +1,16 @@
 
 const RangeBanner = () => {
 	return (
-		<section className="">
-			Hero Range Banner
-		</section>
+		<section className='range__banner jumbotron'>
+			<div className='d-lg-none text-center'>
+				<h1 className='mb-1'>Sunny Honey</h1>
+				<p className='mb-1'>The only safe-tanner you’ll ever need. </p>
+			</div>
+			<picture class="d-block w-100">
+				<source srcset="images/range-hero-banner-dt.jpg" media="(min-width: 992px)" />
+				<img src="images/range-hero-banner-mb.jpg" alt="Placeholder" class="w-100" />
+			</picture>
+        </section>
 	);
 };
 
--- src/UI/templates/TanRange.js
+++ src/UI/templates/TanRange.js
@@ -1,6 +1,17 @@
+import { Fragment } from "react";
+import MagicIRL from "../sections/MagicIRL";
+import RangeBanner from "../sections/RangeBanner";
+import RangeCarousel from "../sections/RangeCarousel";
+import RangeFormula from "../sections/RangeFormula";
+
 const TanRange = () => {
     return (
-        <section className='container'>TanRange</section>
+        <Fragment>
+            <RangeBanner />
+            <RangeCarousel />
+            <RangeFormula />
+            <MagicIRL />
+        </Fragment>
     )
 };
 export default TanRange
