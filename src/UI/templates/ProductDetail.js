5848a0739a8626edea4dcf46bffd5fe2d0a0dbed
Teddy Maulana <teddy.maulana@supernovabrands.com>
2022-03-31 15:12:13 +0700

    clean unused codes

--- src/UI/templates/ProductDetail.js
+++ src/UI/templates/ProductDetail.js
@@ -1,4 +1,4 @@
-import React, { useState, lazy, Suspense } from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Fragment } from 'react';
 import ProductForm from '../sections/ProductForm';
 
@@ -13,23 +13,6 @@ const FaqAccordion = lazy(() => import('../sections/FaqAccordion'));
 const SectionIRL = lazy(() => import('../sections/SectionIRL'));
 
 const ProductDetail = () => {
-    const SHADES = [
-        {
-            id: '32068891541539',
-            text: 'Medium - Subtle glow, lighter skin tones',
-            class: 'medium',
-        },
-        {
-            id: '32068891607075',
-            text: 'Dark - Subtle glow, lighter skin tones',
-            class: 'dark',
-        },
-        {
-            id: '32068891639843',
-            text: 'Ultra Dark - Subtle glow, lighter skin tones',
-            class: 'ultra-dark',
-        }
-    ]
 
     const renderLoader = () => <p>Loading</p>;
 
