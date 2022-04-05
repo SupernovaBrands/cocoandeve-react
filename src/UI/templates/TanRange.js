4a75a313c1e0898ddb714a495f688924abe5ba73
Teddy Maulana <teddy.maulana@supernovabrands.com>
2022-04-05 22:16:42 +0700

    add react lazy

--- src/UI/templates/TanRange.js
+++ src/UI/templates/TanRange.js
@@ -1,26 +1,32 @@
-import { Fragment, lazy } from "react";
-import CustomerReview from "../sections/CustomerReview";
+import { Fragment, lazy, Suspense } from "react";
 
 import RangeBanner from "../sections/RangeBanner";
-import RangeCarousel from "../sections/RangeCarousel";
+// import RangeCarousel from "../sections/RangeCarousel";
 // import RangeFormula from "../sections/RangeFormula";
 // import FaqAccordion from "../sections/FaqAccordion";
-import SectionIRL from "../sections/SectionIRL";
-import SeenIn from "../sections/SeenIn";
+// import SectionIRL from "../sections/SectionIRL";
+// import SeenIn from "../sections/SeenIn";
+// import CustomerReview from "../sections/CustomerReview";
 
+const RangeCarousel = lazy(() => import('../sections/RangeCarousel'));
+const SeenIn = lazy(() => import('../sections/SeenIn'));
 const RangeFormula = lazy(() => import('../sections/RangeFormula'));
 const FaqAccordion = lazy(() => import('../sections/FaqAccordion'));
+const SectionIRL = lazy(() => import('../sections/SectionIRL'));
+const CustomerReview = lazy(() => import('../sections/CustomerReview'));
 
 const TanRange = () => {
     return (
         <Fragment>
             <RangeBanner />
-            <RangeCarousel />
-            <RangeFormula />
-            <SectionIRL />
-            <SeenIn />
-            <FaqAccordion />
-            <CustomerReview />
+            <Suspense fallback={<div></div>}>
+                <RangeCarousel />
+                <RangeFormula />
+                <SectionIRL />
+                <SeenIn />
+                <FaqAccordion />
+                <CustomerReview />
+            </Suspense>
         </Fragment>
     )
 };
