72fcf13a6ccdd4cdbf8de77609f6b7a84123fe1a
Teddy Maulana <teddy.maulana@supernovabrands.com>
2022-04-05 23:44:21 +0700

    update react lazy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import { Route, Routes } from 'react-router-dom';
 import Layout from './UI/layout/Layout';
 import Survey from './UI/templates/Survey';
 
-// import TanRange from "./UI/templates/TanRange";
-// import ProductDetail from "./UI/templates/ProductDetail";
+import TanRange from "./UI/templates/TanRange";
+import ProductDetail from "./UI/templates/ProductDetail";
 
-const TanRange = lazy(() => import('./UI/templates/TanRange'));
-const ProductDetail = lazy(() => import('./UI/templates/ProductDetail'));
+// const TanRange = lazy(() => import('./UI/templates/TanRange'));
+// const ProductDetail = lazy(() => import('./UI/templates/ProductDetail'));
 
 const App = () => {
   const noHeader = () => window.location.pathname === '/survey';
@@ -15,14 +15,12 @@ const App = () => {
 
   return (
     <Layout noFooter={noFooter()} noHeader={noHeader()}>
-      <Suspense fallback={<div></div>}>
         <Routes>
           <Route path='/tan-range' element={<TanRange />} />
           <Route path='/products/:handle' element={<ProductDetail />} />
           <Route path='/sunny-honey-bali-bronzing-self-tan-set' element={<ProductDetail />} />
           <Route path='/survey' element={<Survey />} />
         </Routes>
-      </Suspense>
     </Layout>
   )
 };
