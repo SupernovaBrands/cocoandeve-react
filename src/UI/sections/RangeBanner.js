2b8a9bfb73dcdaace8668c660a178894b62ee3f8
rizkiaprilian <rizki.a.aprilian@gmail.com>
2022-04-05 13:35:28 +0700

    implement webp responsive

--- src/UI/sections/RangeBanner.js
+++ src/UI/sections/RangeBanner.js
@@ -7,9 +7,24 @@ const RangeBanner = () => {
 				<p className='mb-1'>The only safe-tanner you’ll ever need. </p>
 			</div>
 			<picture className="d-block w-100">
-				<source type="image/webp" srcSet={`${process.env.PUBLIC_URL}/compressed_images/range-hero-banner-dt.webp`} media="(min-width: 992px)" />
-				<source type="image/jpeg" srcSet={`${process.env.PUBLIC_URL}/images/range-hero-banner-dt.jpg`} media="(min-width: 992px)" />
-				<img src={`${process.env.PUBLIC_URL}/images/range-hero-banner-mb.jpg`} alt="Placeholder" className="w-100" />
+				{/* load webp in different sizes if browser supports it */}
+				<source
+					type="image/webp"
+					srcSet={`${process.env.PUBLIC_URL}/compressed_images/range-hero-banner-mb.webp 828w, ${process.env.PUBLIC_URL}/compressed_images/range-hero-banner-dt.webp 992w`}/>
+				{/* load jpg in different sizes if browser doesn't support webp */}
+				<source
+					type="image/jpeg"
+					srcSet={`${process.env.PUBLIC_URL}/images/range-hero-banner-mb.jpg 828w, ${process.env.PUBLIC_URL}/images/range-hero-banner-dt.jpg 992w`} />
+
+				<img
+					srcSet={`
+						${process.env.PUBLIC_URL}/compressed_images/range-hero-banner-mb.jpg 828w,
+						${process.env.PUBLIC_URL}/compressed_images/range-hero-banner-dt.jpg 992w`}
+					sizes="(max-width: 991px) 828px, 992px"
+
+					src={`${process.env.PUBLIC_URL}/images/range-hero-banner-mb.jpg`}
+					alt="Placeholder"
+					className="w-100 embed-responsive-item fit--cover" />
 			</picture>
         </section>
 	);
