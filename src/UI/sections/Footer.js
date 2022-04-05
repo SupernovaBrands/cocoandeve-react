83ee0b72df981af1531c5c97d686114f508b58cd
rizkiaprilian <rizki.a.aprilian@gmail.com>
2022-04-05 10:19:59 +0700

    update footer & review links

--- src/UI/sections/Footer.js
+++ src/UI/sections/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<footer className='pt-md-6 pb-md-6 pt-2 pt-lg-4 pb-1 bg-white text-body'>
+		<footer className='pt-md-6 pb-md-6 pt-2 pt-lg-4 pb-1 bg-secondary text-white'>
 			<div className='container px-g'>
 				<hr />
 				<p className='d-flex justify-content-center align-items-baseline'>© 2022 Coco&amp;Eve </p>
