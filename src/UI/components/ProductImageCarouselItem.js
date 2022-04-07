373b549e2c021bf7277190916d0c3ea18e466945
Teddy Maulana <teddy.maulana@supernovabrands.com>
2022-04-07 23:09:18 +0700

    update images links

--- src/UI/components/ProductImageCarouselItem.js
+++ src/UI/components/ProductImageCarouselItem.js
@@ -3,11 +3,11 @@ const ProductImageCarouselItem = (props) => {
     return (
         <div className={`carousel-item ${props.index === 0 ? 'active': ''}`}>
             <picture>
-                <source type="image/webp" srcSet={`${props.webp}/productmobile`}/>
-                <source type="image/webp" srcSet={`${props.webp}/productdesktop`} media="(min-width: 992px)"/>
-                <source type="image/jpeg" srcSet={`${props.jpg}/productmobile`}/>
-                <source type="image/jpeg" srcSet={`${props.jpg}/productdesktop`} media="(min-width: 992px)"/>
-                <img src={`${props.jpg}/productdesktop`} alt="" className="w-100 embed-responsive-item fit--cover"/>
+                <source type="image/webp" srcSet={`${props.webp}/828x`}/>
+                <source type="image/webp" srcSet={`${props.webp}/1140x`} media="(min-width: 992px)"/>
+                <source type="image/jpeg" srcSet={`${props.jpg}/828x`}/>
+                <source type="image/jpeg" srcSet={`${props.jpg}/1140x`} media="(min-width: 992px)"/>
+                <img src={`${props.jpg}/1140x`} alt="" className="w-100 embed-responsive-item fit--cover"/>
             </picture>
         </div>
     );
