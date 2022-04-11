87e035e67d1e9bf0dd8e2217085e8bf2d4cc3074
rizkiaprilian <rizki.a.aprilian@gmail.com>
2022-04-11 11:16:21 +0700

    update meta desc

--- src/UI/sections/RangeBanner.js
+++ src/UI/sections/RangeBanner.js
@@ -8,8 +8,8 @@ const RangeBanner = () => {
 			</div>
 
 			<picture className="d-block w-100">
-				<source srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/432fb9c7-48a8-448b-9598-6cff8051e500/1140x" media="(min-width: 768px)" type="image/webp" />
-				<source srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/8fc7c523-06a3-4408-a6b3-84b79f84c500/1140x" media="(min-width: 768px)" />
+				<source srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/432fb9c7-48a8-448b-9598-6cff8051e500/1400x" media="(min-width: 768px)" type="image/webp" />
+				<source srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/8fc7c523-06a3-4408-a6b3-84b79f84c500/1400x" media="(min-width: 768px)" />
 				<source srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/383dbdf2-f7e4-412b-8447-54dbb0984a00/828x" type="image/webp" />
 				<img
 					src="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/afa85196-4f02-42da-091e-393e63d1c900/828x"
