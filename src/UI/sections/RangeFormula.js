91e32c7c93a4b678177e58c5598b868dad4c47ab
rizkiaprilian <rizki.a.aprilian@gmail.com>
2022-04-12 12:06:41 +0700

    update formula

--- src/UI/sections/RangeFormula.js
+++ src/UI/sections/RangeFormula.js
@@ -1,7 +1,7 @@
-import { ReactComponent as FormulaList1 } from '../../assets/formula-list-1.svg';
-import { ReactComponent as FormulaList2 } from '../../assets/formula-list-2.svg';
-import { ReactComponent as FormulaList3 } from '../../assets/formula-list-3.svg';
-import { ReactComponent as FormulaList4 } from '../../assets/formula-list-4.svg';
+import { ReactComponent as FormulaList11 } from '../../assets/formula-list-1.svg';
+import { ReactComponent as FormulaList21 } from '../../assets/formula-list-2.svg';
+import { ReactComponent as FormulaList31 } from '../../assets/formula-list-3.svg';
+import { ReactComponent as FormulaList41 } from '../../assets/formula-list-4.svg';
 
 const RangeFormula = () => {
 	return (
@@ -13,19 +13,19 @@ const RangeFormula = () => {
 						<p className='h4 text-center text-lg-start mb-0 fw-normal'>NO nasties, NO orange tones.</p>
 						<ul className='list-unstyled row mt-2 mb-0 range__formula-list'>
 							<li className='col-12 d-flex align-items-center mb-2'>
-								<FormulaList1 className='me-g d-flex flex-shrink-0 justify-content-center' />
+								<FormulaList11 className='me-g d-flex flex-shrink-0 justify-content-center' />
 								Blurs pigmentation and perfects skin.
 							</li>
 							<li className='col-12 d-flex align-items-center mb-2'>
-								<FormulaList2 className='me-g d-flex flex-shrink-0 justify-content-center' />
+								<FormulaList21 className='me-g d-flex flex-shrink-0 justify-content-center' />
 								Tropical mango and guava scent<br />(no biscuit smell!)
 							</li>
 							<li className='col-12 d-flex align-items-center mb-2'>
-								<FormulaList3 className='me-g d-flex flex-shrink-0 justify-content-center' />
+								<FormulaList31 className='me-g d-flex flex-shrink-0 justify-content-center' />
 								Lightweight, non-sticky formula.
 							</li>
 							<li className='col-12 d-flex align-items-center'>
-								<FormulaList4 className='me-g d-flex flex-shrink-0 justify-content-center' />
+								<FormulaList41 className='me-g d-flex flex-shrink-0 justify-content-center' />
 								Developed with a green-grey base for a natural looking, golden glow with no orangey tones.
 							</li>
 						</ul>
