43673b6a757e50a56e4a9cadd555796ca4d9fca0
rizkiaprilian <rizki.a.aprilian@gmail.com>
2022-04-12 10:38:23 +0700

    test carousel

--- src/UI/templates/TanRange.js
+++ src/UI/templates/TanRange.js
@@ -2,14 +2,14 @@ import React, { Fragment, lazy, Suspense } from 'react';
 
 import RangeBanner from "../sections/RangeBanner";
 import '../../range.scss';
-// import RangeCarousel from "../sections/RangeCarousel";
+import RangeCarousel from "../sections/RangeCarousel";
 // import RangeFormula from "../sections/RangeFormula";
 // import FaqAccordion from "../sections/FaqAccordion";
 // import SectionIRL from "../sections/SectionIRL";
 // import SeenIn from "../sections/SeenIn";
 // import CustomerReview from "../sections/CustomerReview";
 
-const RangeCarousel = lazy(() => import('../sections/RangeCarousel'));
+// const RangeCarousel = lazy(() => import('../sections/RangeCarousel'));
 const SeenIn = lazy(() => import('../sections/SeenIn'));
 const RangeFormula = lazy(() => import('../sections/RangeFormula'));
 const FaqAccordion = lazy(() => import('../sections/FaqAccordion'));
