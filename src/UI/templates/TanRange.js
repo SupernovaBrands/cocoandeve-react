fa842cd01715212ecca489fdf6db93ec3d945562
rizkiaprilian <rizki.a.aprilian@gmail.com>
2022-04-12 12:45:36 +0700

    remove import

--- src/UI/templates/TanRange.js
+++ src/UI/templates/TanRange.js
@@ -3,7 +3,7 @@ import React, { Fragment, lazy, Suspense } from 'react';
 import RangeBanner from "../sections/RangeBanner";
 import '../../range.scss';
 import RangeCarousel from "../sections/RangeCarousel";
-import ProductForm from '../sections/ProductForm';
+// import ProductForm from '../sections/ProductForm';
 // import RangeFormula from "../sections/RangeFormula";
 // import FaqAccordion from "../sections/FaqAccordion";
 // import SectionIRL from "../sections/SectionIRL";
