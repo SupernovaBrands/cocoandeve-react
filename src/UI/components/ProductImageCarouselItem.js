26e9685b81a3483aa1d1d89d536a92834ad08d7e
Teddy Maulana <teddy.maulana@supernovabrands.com>
2022-04-13 10:44:05 +0700

    remove lazyload

--- src/UI/components/ProductImageCarouselItem.js
+++ src/UI/components/ProductImageCarouselItem.js
@@ -6,7 +6,7 @@ const ProductImageCarouselItem = (props) => {
                 <source type="image/webp" srcSet={`${props.webp}/1140x`} media="(min-width: 992px)"/>
                 <source type="image/jpeg" srcSet={`${props.jpg}/828x`}/>
                 <source type="image/jpeg" srcSet={`${props.jpg}/1140x`} media="(min-width: 992px)"/>
-                <img src={`${props.jpg}/828x`} alt="" className="w-100 embed-responsive-item fit--cover" loading="lazy" />
+                <img src={`${props.jpg}/828x`} alt="" className="w-100 embed-responsive-item fit--cover"  />
             </picture>
         </div>
     );
