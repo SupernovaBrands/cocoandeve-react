38a5569bc9c600cb2561fcc12b370551e4b34366
rizkiaprilian <rizki.a.aprilian@gmail.com>
2022-04-14 10:02:30 +0700

    update svg icons

--- src/UI/sections/RangeFormula.js
+++ src/UI/sections/RangeFormula.js
@@ -1,7 +1,7 @@
-import { ReactComponent as FormulaList11 } from '../../assets/formula-list-1.svg';
-import { ReactComponent as FormulaList21 } from '../../assets/formula-list-2.svg';
-import { ReactComponent as FormulaList31 } from '../../assets/formula-list-3.svg';
-import { ReactComponent as FormulaList41 } from '../../assets/formula-list-4.svg';
+import { ReactComponent as FormulaList11 } from '../../assets/skin-protection.svg';
+import { ReactComponent as FormulaList21 } from '../../assets/guava.svg';
+import { ReactComponent as FormulaList31 } from '../../assets/diet.svg';
+import { ReactComponent as FormulaList41 } from '../../assets/clean.svg';
 
 const RangeFormula = () => {
 	return (
