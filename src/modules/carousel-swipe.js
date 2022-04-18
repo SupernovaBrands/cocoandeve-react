f629f3b24563e0c58cf7bedc67466829a0afcad0
Teddy Maulana <teddy.maulana@supernovabrands.com>
2022-04-18 13:27:27 +0700

    add carousel swipe js

---
+++ src/modules/carousel-swipe.js
@@ -0,0 +1,51 @@
+import bootstrap from "../vendor/bootstrap.bundle";
+const carouselSwipe = (carouselId) => {
+    setTimeout(() => {
+        const carouselSwipe = document.getElementById("customerReviews");
+        const carousel = new bootstrap.Carousel(carouselSwipe);
+
+        let xDown = null;
+		let yDown = null;
+
+        function getTouches(evt) {
+			return evt.touches || evt.originalEvent.touches;
+		}
+
+        function handleTouchStart(evt) {
+			const firstTouch = getTouches(evt)[0];
+			xDown = firstTouch.clientX;
+			yDown = firstTouch.clientY;
+            console.log('xDOwn', xDown)
+            console.log('yDOwn', yDown)
+		}
+
+        function handleTouchMove(evt) {
+			if (!xDown || !yDown) {
+				return;
+			}
+
+            const xUp = evt.touches[0].clientX;
+			const yUp = evt.touches[0].clientY;
+
+			const xDiff = xDown - xUp;
+			const yDiff = yDown - yUp;
+
+            if (Math.abs(xDiff) > Math.abs(yDiff)) {
+
+                if (xDiff > 0) {
+                    carousel.next();
+                } else {
+                    carousel.prev();
+                }
+            } else {
+                xDown = null;
+				yDown = null;
+            }
+        }
+
+        carouselSwipe.addEventListener('touchstart', handleTouchStart, false);
+		carouselSwipe.addEventListener('touchmove', handleTouchMove, false);
+    }, 500);
+};
+
+export default carouselSwipe;
