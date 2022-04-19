b49c1b1a2b2a8fbb109778bbb8f8e193fb2756ba
Teddy Maulana <teddy.maulana@supernovabrands.com>
2022-04-19 11:50:05 +0700

    removelogs

--- src/UI/components/ProductImageCarouselItem.js
+++ src/UI/components/ProductImageCarouselItem.js
@@ -3,7 +3,6 @@ import { useEffect } from "react";
 const ProductImageCarouselItem = (props) => {
     const removeShimmer = (img) => {
         // eslint-disable-next-line no-param-reassign
-        console.log('remove shimmer')
         img.onload = null;
         const parent = img.closest('.bg-shimmer');
         parent.classList.add('bg-shimmer--loaded');
@@ -14,7 +13,6 @@ const ProductImageCarouselItem = (props) => {
 
     useEffect(() => {
         document.querySelectorAll('.bg-shimmer img').forEach((img) => {
-            console.log('bg shimmer load', img)
             if (img.complete) {
                 removeShimmer(img);
             } else {
