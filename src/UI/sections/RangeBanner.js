0940f50659d4d61251e44f316258e355f1f84462
rizkiaprilian <rizki.a.aprilian@gmail.com>
2022-04-22 09:33:32 +0700

    fix type jpeg

--- src/UI/sections/RangeBanner.js
+++ src/UI/sections/RangeBanner.js
@@ -9,13 +9,13 @@ const RangeBanner = () => {
 
 			<picture className="d-block w-100">
 				<source srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/e597aaa2-22fb-416e-0572-2671dd39f000/1400x" media="(min-width: 768px)" type="image/webp" />
-				<source srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/a8d30ba5-6fc6-41ee-5462-cc9bbaacd800/1400x" media="(min-width: 768px)" />
+				<source srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/a8d30ba5-6fc6-41ee-5462-cc9bbaacd800/1400x" media="(min-width: 768px)" type="image/jpeg" />
 				<source srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/383dbdf2-f7e4-412b-8447-54dbb0984a00/828x" type="image/webp" />
 				<img
 					src="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/afa85196-4f02-42da-091e-393e63d1c900/828x"
 					srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/383dbdf2-f7e4-412b-8447-54dbb0984a00/828x"
 					alt="Range banner"
-					className="w-100 embed-responsive-item fit--cover" />
+					className="w-100 fit--cover" />
 			</picture>
         </section>
 	);
