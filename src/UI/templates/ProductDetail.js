c058d5955912434a88c53cfd9e0178d70dc41c01
Teddy Maulana <teddy.maulana@supernovabrands.com>
2022-05-11 16:15:06 +0700

    add isabtest codes

--- src/UI/templates/ProductDetail.js
+++ src/UI/templates/ProductDetail.js
@@ -3,6 +3,7 @@ import ProductForm from '../sections/ProductForm';
 import '../../product.scss';
 import Carousel from '../components/Carousel';
 import ProductProvider from '../../store/ProductProvider';
+import { isABTest } from "../../modules/Utils";
 
 // import FaqAccordion from '../sections/FaqAccordion';
 // import SeenIn from '../sections/SeenIn';
@@ -13,6 +14,18 @@ const CustomerReview = lazy(() => import('../sections/CustomerReview'));
 const SeenIn = lazy(() => import('../sections/SeenIn'));
 const FaqAccordion = lazy(() => import('../sections/FaqAccordion'));
 const SectionIRL = lazy(() => import('../sections/SectionIRL'));
+
+isABTest('o3JFdSN0S1Ogpio5tWJmBg').then(({ test, value }) => {
+    if (test && value === '1') {
+        console.log('variant', value);
+        var main = document.querySelector('main');
+        var cs = document.querySelector('.customer-reviews');
+        var faq = document.querySelector('.faq-accordion');
+        faq.classList.add('mt-2');
+        faq.classList.add('mt-lg-4');
+        main.insertBefore(faq, cs);
+    }
+});
 
 const ProductDetail = (props) => {
     return (
