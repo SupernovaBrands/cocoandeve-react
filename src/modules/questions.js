635c297aa25401a252e2ce26526af4fa323d0767
pebiantara <pebiantara@gmail.com>
2022-05-31 15:41:45 +0700

    Survey Quiz Translations

--- src/modules/questions.js
+++ src/modules/questions.js
@@ -1,24 +1,58 @@
 const Questions = [
     {
-        question: "What kind of tan do you want to have?",
-        answers: [
+        question: {
+            en: 'What kind of tan do you want to have?',
+            de: 'Welche Bräune möchtest du haben?',
+            fr: "Quel type de bronzage souhaitez-vous obtenir?"
+        },
+        answers: {
+            en: [
             'Natural',
             'Sun-kissed and fresh from Holiday',
             'Deep, bronzed, and at least 2 levels darker than my original',
-        ],
+            ],
+            de: [
+                'Natürlich',
+                'Sonnengeküsst und frisch aus dem Urlaub',
+                'Tief, gebräunt und mindestens 2 Stufen dunkler als meine original Hautfarbe',
+            ],
+            fr: [
+                'Naturel',
+                'Bronzé et fraîchement revenu de vacances',
+                'Intense, au moins deux fois plus foncé que mon teint naturel',
+            ],
+        },
         type: 'SingleChoice',
         buttonType: 'full',
     },
     {
-        question: "What’s your skin tone?",
-        answers: [
+        question: {
+            en: "What’s your skin tone?",
+            de: 'Was ist deine Hautfarbe?',
+            fr: 'Quelle est votre carnation ?',
+        },
+        answers: {
+            en:[
             'Fair',
             'Light',
             'Medium',
             'Dark',
-        ],
+            ],
+            de: [
+                'Sehr hell',
+                'Hell',
+                'Mittel',
+                'Dunkel',
+            ],
+            fr: [
+                'Très claire',
+                'Claire',
+                'Médium',
+                'Foncée',
+            ],
+        },
         images: [
-            { 
+            {
                 jpg: 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/5ae2dd27-d9fe-4977-ac17-7f9fed94eb00/828x',
                 webp: 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/a071a974-5d70-4eb2-1cdf-b3fa5a86ec00/828x',
             },
@@ -38,45 +72,116 @@ const Questions = [
         type: 'SingleChoiceImage',
     },
     {
-        question: 'Which areas do you want to tan?',
-        answers: [
+        question: {
+            en: 'Which areas do you want to tan?',
+            fr: 'Quelles sont les zones que vous souhaitez bronzer ?',
+            de: 'Welche Bereiche möchtest du bräunen?',
+        },
+        answers: {
+            en: [
             'Face',
             'Body',
             'Both',
-        ],
+            ],
+            fr: [
+                'Visage',
+                'Corps',
+                'Les deux',
+            ],
+            de: [
+                'Gesicht',
+                'Körper',
+                'Beides',
+            ]
+        },
         type: 'SingleChoice',
         buttonType: 'full',
-    },    
+    },
     {
-        question: "Have you used self-tanner before?",
-        answers: [
+        question: {
+            en: "Have you used self-tanner before?",
+            de: 'Hast du schon mal Selbstbräuner verwendet?',
+            fr: "Avez-vous déjà utilisé de l'autobronzant ?",
+        },
+        answers: {
+            en: [
             "Yes, I've tried them all!",
             "I have, I am occasionally tanning with self tanner",
             "No, this would be my first",
-        ],
+            ],
+            de: [
+                'Ja, ich habe sie alle ausprobiert!',
+                'Ja, ich bräune mich gelegentlich mit Selbstbräuner',
+                'Nein, das wäre mein erstes Mal',
+            ],
+            fr: [
+                'Oui, je les ai tous testés !',
+                "Oui, j'utilise de l'autobronzant de temps en temps.",
+                'Non, ce serait la première fois',
+            ],
+        },
         type: 'SingleChoice',
         buttonType: 'full',
     },
     {
-        question: "What brands are you using right now?",
-        answers: [
-            {label: 'Coco & Eve', type: 'checkbox'},
-            {label: 'Bali Body', type: 'checkbox'},
-            {label: 'Bondi Sands', type: 'checkbox'},
-            {label: 'b.tan', type: 'checkbox'},
-            {label: 'St. Tropez', type: 'checkbox'},
-            {label: 'Coola', type: 'checkbox'},
-            {label: 'Isle Paradise', type: 'checkbox'},
-            {label: 'Tan-Luxe', type: 'checkbox'},
-            {label: 'Loving Tan', type: 'checkbox'},
-            {label: 'Other', type: 'input'},
-        ],
-        caption: 'Choose all that apply!',
+        question: {
+            en: "What brands are you using right now?",
+            fr: "Quelles marques utilisez-vous en ce moment ?",
+            de: "Welche Marken verwendest du gerade?",
+        },
+        answers: {
+            en: [
+                {label: 'Coco & Eve', type: 'checkbox'},
+                {label: 'Bali Body', type: 'checkbox'},
+                {label: 'Bondi Sands', type: 'checkbox'},
+                {label: 'b.tan', type: 'checkbox'},
+                {label: 'St. Tropez', type: 'checkbox'},
+                {label: 'Coola', type: 'checkbox'},
+                {label: 'Isle Paradise', type: 'checkbox'},
+                {label: 'Tan-Luxe', type: 'checkbox'},
+                {label: 'Loving Tan', type: 'checkbox'},
+                {label: 'Other', type: 'input'},
+            ],
+            de: [
+                {label: 'Coco & Eve', type: 'checkbox'},
+                {label: 'Bali Body', type: 'checkbox'},
+                {label: 'Bondi Sands', type: 'checkbox'},
+                {label: 'b.tan', type: 'checkbox'},
+                {label: 'St. Tropez', type: 'checkbox'},
+                {label: 'Coola', type: 'checkbox'},
+                {label: 'Isle Paradise', type: 'checkbox'},
+                {label: 'Tan-Luxe', type: 'checkbox'},
+                {label: 'Loving Tan', type: 'checkbox'},
+                {label: 'Sonstiges', type: 'input'},
+            ],
+            fr: [
+                {label: 'Coco & Eve', type: 'checkbox'},
+                {label: 'Bali Body', type: 'checkbox'},
+                {label: 'Bondi Sands', type: 'checkbox'},
+                {label: 'b.tan', type: 'checkbox'},
+                {label: 'St. Tropez', type: 'checkbox'},
+                {label: 'Coola', type: 'checkbox'},
+                {label: 'Isle Paradise', type: 'checkbox'},
+                {label: 'Tan-Luxe', type: 'checkbox'},
+                {label: 'Loving Tan', type: 'checkbox'},
+                {label: 'Autre', type: 'input'},
+            ]
+        },
+        caption: {
+            en: 'Choose all that apply!',
+            de: 'Wähle alle, die zutreffen!',
+            fr: "Choisissez toutes les raisons qui vous concernent !",
+        },
         type: 'MultipleChoice',
     },
     {
-        question: "What do you like in a self-tanner?",
-        answers: [
+        question: {
+            en: "What do you like in a self-tanner?",
+            de: 'Was magst du an einem Selbstbräuner?',
+            fr: "Qu'est-ce que vous aimez dans un autobronzant ?",
+        },
+        answers: {
+            en: [
             {label: 'Easy application', type: 'checkboxNumber'},
             {label: 'Non-drying', type: 'checkboxNumber'},
             {label: 'No orange tones', type: 'checkboxNumber'},
@@ -84,48 +189,137 @@ const Questions = [
             {label: 'Non-sticky, and light weight', type: 'checkboxNumber'},
             {label: 'Vegan and Cruelty-free', type: 'checkboxNumber'},
             {label: "This doesn't apply to me", type: 'checkboxAll'},
-        ],
-        caption: 'Choose up to 6 in order of priority',
+            ],
+            de: [
+                {label: 'Einfache Anwendung', type: 'checkboxNumber'},
+                {label: 'Keine Orangetöne', type: 'checkboxNumber'},
+                {label: 'Nicht klebrig und leicht', type: 'checkboxNumber'},
+                {label: 'Das trifft auf mich nicht zu', type: 'checkboxNumber'},
+                {label: 'Nicht trocknend', type: 'checkboxNumber'},
+                {label: 'Keine unangenehmen Bräunungsgerüche', type: 'checkboxNumber'},
+                {label: "Vegan und tierversuchsfrei", type: 'checkboxAll'},
+            ],
+            fr: [
+                {label: 'Application facile', type: 'checkboxNumber'},
+                {label: 'Pas de sous-tons orangés', type: 'checkboxNumber'},
+                {label: 'Léger et non collant', type: 'checkboxNumber'},
+                {label: 'Cela ne me concerne pas', type: 'checkboxNumber'},
+                {label: 'Ne sèche pas', type: 'checkboxNumber'},
+                {label: 'Pas de mauvaises senteurs', type: 'checkboxNumber'},
+                {label: "Vegan et non testé sur les animaux", type: 'checkboxAll'},
+            ]
+        },
+        caption: {
+            en: 'Choose up to 6 in order of priority',
+            de: 'Wähle bis zu 6 Antworten in der Reihenfolge deiner Priorität aus',
+            fr: "Sélectionnez jusqu'à 6 critères par ordre de priorité",
+        },
         type: 'MultipleChoice',
         lastFull: true,
         maxChoose: 6,
         lastDisableForAll: true,
     },
     {
-        question: "Do you like looking tanned?",
-        answers: [
-            "Occasionally",
-            "I love a post holiday glow",
-            "I am always looking tanned",
-        ],
+        question: {
+            en: "Do you like looking tanned?",
+            de: "Siehst du gerne gebräunt aus?",
+            fr: "Aimez-vous paraître bronzé ?",
+        },
+        answers: {
+            en: [
+                "Occasionally",
+                "I love a post holiday glow",
+                "I am always looking tanned",
+            ],
+            fr: [
+                "De temps en temps",
+                "J'aime avoir l'air bronzé après les vacances",
+                "Je suis toujours bronzé",
+            ],
+            de: [
+                "Hin und wieder",
+                "Ich liebe einen Urlaubs-Glow",
+                "Ich sehe immer gebräunt aus",
+            ],
+        },
         type: 'SingleChoice',
         buttonType: 'full',
     },
     {
-        question: "What are the reasons you want to use a self-tanner right now?",
-        answers: [
-            {label: 'Holiday', type: 'checkbox'},
-            {label: 'Party', type: 'checkbox'},
-            {label: 'Workout', type: 'checkbox'},
-            {label: 'Better weather', type: 'checkbox'},
-            {label: 'Look healthier', type: 'checkbox'},
-            {label: 'Even out skin tone', type: 'checkbox'},
-            {label: 'Other', type: 'input'},
-        ],
+        question: {
+            en: "What are the reasons you want to use a self-tanner right now?",
+            de: "Was sind die Gründe, warum du jetzt einen Selbstbräuner verwenden möchtest?",
+            fr: "Quelles sont les raisons qui vous poussent à utiliser un autobronzant en ce moment ?",
+        },
+        answers: {
+            en: [
+                {label: 'Holiday', type: 'checkbox'},
+                {label: 'Party', type: 'checkbox'},
+                {label: 'Workout', type: 'checkbox'},
+                {label: 'Better weather', type: 'checkbox'},
+                {label: 'Look healthier', type: 'checkbox'},
+                {label: 'Even out skin tone', type: 'checkbox'},
+                {label: 'Other', type: 'input'},
+            ],
+            de: [
+                {label: 'Urlaub', type: 'checkbox'},
+                {label: 'Party', type: 'checkbox'},
+                {label: 'Trainieren', type: 'checkbox'},
+                {label: 'Besseres Wetterr', type: 'checkbox'},
+                {label: 'Gesünder aussehen', type: 'checkbox'},
+                {label: 'Hautton ausgleichen', type: 'checkbox'},
+                {label: 'Sonstiges', type: 'input'},
+            ],
+            fr: [
+                {label: 'Vacances', type: 'checkbox'},
+                {label: 'Soirée', type: 'checkbox'},
+                {label: "Faire du sport", type: 'checkbox'},
+                {label: 'Meilleure météo', type: 'input'},
+                {label: "Avoir l'air en meilleure forme", type: 'checkbox'},
+                {label: 'Unifier son teint', type: 'checkbox'},
+                {label: 'Autre', type: 'checkbox'},
+            ],
+        },
         type: 'MultipleChoice',
-        caption: 'Choose all that apply!',
+        caption: {
+            en: 'Choose all that apply!',
+            de: 'Wähle alle, die zutreffen!',
+            fr: 'Choisissez toutes les raisons qui vous concernent !',
+        },
         lastFull: true,
     },
     {
-        question: "What's your age?",
-        answers: [
+        question: {
+            en: "What's your age?",
+            de: "Wie alt bist du?",
+            fr: "Quel âge avez-vous ?",
+        },
+        answers: {
+            en: [
             '18-24',
             '25-34',
             '35-44',
             '45-54',
             '55-64',
             '65 up',
-        ],
+            ],
+            de: [
+            '18-24',
+            '25-34',
+            '35-44',
+            '45-54',
+            '55-64',
+            '65 up',
+            ],
+            fr: [
+            '18-24',
+            '25-34',
+            '35-44',
+            '45-54',
+            '55-64',
+            '65 up',
+            ],
+        },
         type: 'SingleChoice',
     },
 ];
---
+++ src/modules/translations.js
@@ -0,0 +1,37 @@
+const Translations = {
+    en: {
+        heading: 'Find your perfect tan \nin 90 seconds!',
+        subheading: 'Take the Tan matching quiz to find your perfect shade of gorgeous glow. It only takes 90 seconds to find your true colour match.',
+        caption: 'Your perfect shade is just a few clicks away',
+        btn: {
+            start: 'Take the Quiz',
+            next: 'Next',
+            prev: 'Previous Question',
+            result: 'See results!',
+        }
+    },
+    fr: {
+        heading: "Trouvez votre teinte d'autobronzant parfaite \nen 90 secondes !",
+        subheading: "Faites le quiz de compatibilité d'autobronzant pour trouver la teinte qui vous correspond le mieux. 90 secondes suffisent pour trouver la nuance parfaite.",
+        caption: "Votre teinte parfaite n'est plus qu'à quelques clics.",
+        btn: {
+            start: 'Faire le quiz',
+            next: 'Suivant',
+            prev: 'Question précédente',
+            result: 'Voir les résultats !',
+        }
+    },
+    de: {
+        heading: "Finde deine perfekte Bräune \nin 90 Sekunden!",
+        subheading: "Nimm am Selbstbräuner-Quiz teil, um deine perfekte Bräunungs-Nuance für einen wunderschönen Glow zu finden. Es dauert nur 90 Sekunden, um deine wahre Farbübereinstimmung zu finden.",
+        caption: 'Deine perfekte Nuance ist nur ein paar Klicks entfernt',
+        btn: {
+            start: 'Nimm an unserem Quiz teil',
+            next: 'Weiter',
+            prev: 'Vorherige Frage',
+            result: 'Siehe Ergebnisse!',
+        }
+    }
+}
+
+export default Translations;
