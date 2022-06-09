03b2010a242bd51c9d54e3a65dbf829a7cbbea7b
rizkiaprilian <rizki.a.aprilian@gmail.com>
2022-06-09 14:55:51 +0700

    test carousel swipe

--- src/vendor/bootstrap.bundle.js
+++ src/vendor/bootstrap.bundle.js
@@ -4,7 +4,7 @@
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
   */
 
-export const { Carousel } = require("bootstrap");
+// export const { Carousel } = require("bootstrap");
 
 (function (global, factory) {
   // eslint-disable-next-line no-unused-expressions
