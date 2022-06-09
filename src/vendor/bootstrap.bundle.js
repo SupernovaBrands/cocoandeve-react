a9817fabb296ce4d4f54e39cdc36c3bd19822cb9
rizkiaprilian <rizki.a.aprilian@gmail.com>
2022-06-09 14:25:03 +0700

    update carousel export

--- src/vendor/bootstrap.bundle.js
+++ src/vendor/bootstrap.bundle.js
@@ -4,8 +4,6 @@
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
   */
 
-export const { Carousel } = require("bootstrap");
-
 (function (global, factory) {
   // eslint-disable-next-line no-unused-expressions
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
@@ -4167,6 +4165,7 @@ export const { Carousel } = require("bootstrap");
 
   defineJQueryPlugin(Tab);
 
+  module.export = Carousel;
 
   /**
    * --------------------------------------------------------------------------
