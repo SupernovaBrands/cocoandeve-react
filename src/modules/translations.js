3e15bd5806d150534fd8034bab6b2fbb85e08a88
Teddy Maulana <maulanateddy99@gmail.com>
2022-11-07 08:58:41 +0700

    update copies

--- src/modules/translations.js
+++ src/modules/translations.js
@@ -143,6 +143,19 @@ const Translations = {
             submit: 'Register now!'
         }
     },
+    uk: {
+        sweepstakes: {
+            heading: 'Get early access to the biggest sale of the year!',
+            subHeading: 'Pre-register now, plus stand a chance to win $1000 of salon services and your Coco & Eve wishlist!',
+            emailPh: 'Email',
+            phonePh: 'Phone number',
+            toc: 'By signing up you agree to receive exclusive offers via email or recurring automated marketing messages. For more info see our Privacy Policy. Message frequency varies. Msg & data rates may apply. Sign up not required for purchase.',
+            ty_heading: 'You’re registered!',
+            ty_subHeading: 'Stay tuned to receive your early access code on November 14, and find out if you’re a winner via email or SMS!',
+            ty_note: '*Customers who sign up to our presale through SMS or email address will be given a chance to win up to $500 USD worth of products as well as $1000 salon services. Contest winners will be notified via SMS or email. Pre sale registration and contest will run until November 13, 2022 21:00 EST. Prizes cannot be used in conjunction with any other code, offer or promotion. Coco & Eve reserves the right to cancel or alter any promotion without prior notice. In the event of any dispute, the decision of Coco & Eve is final. No purchase necessary to enter or win. Each participant is only allowed one entry.',
+            submit: 'Register now!'
+        }
+    },
     ca: {
         sweepstakes: {
             heading: 'Get early access to the biggest sale of the year! ',
