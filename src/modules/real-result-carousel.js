87c383c287fc5588ca5e3ac351528006da22f245
rizkiaprilian <rizki.a.aprilian@gmail.com>
2023-05-09 14:00:08 +0700

    amazon LP

--- src/modules/real-result-carousel.js
+++ src/modules/real-result-carousel.js
@@ -1,12 +1,84 @@
 const REAL_RESULT_DATA = {
 	hair: [
 		{
-			img: 'https://cdn.shopify.com/s/files/1/0225/4641/5693/files/Review_7_592x.png',
-			srcSet: 'https://cdn.shopify.com/s/files/1/0225/4641/5693/files/Review_7_530x.png',
+			img: '//cdn.shopify.com/s/files/1/0225/4641/5693/files/Review_6_592x.jpg',
+			srcSet: '//cdn.shopify.com/s/files/1/0225/4641/5693/files/Review_6_530x.jpg',
+			productHandle: 'hydrating-shampoo-conditioner-bundle',
+			productTitle: 'Shampoo & Conditioner',
+			content: 'I started using the Coco & Eve hydrating shampoo and conditioner two weeks ago. I absolutely love it! My frizz is a lot less, my scalp is clean after just one shampoo and rinse, and my hair looks so soft and shiny. It smells amazing too! I will definitely continue using the product, I absolutely love it!',
+			name: '@Athina'
+		},
+		{
+			img: '//cdn.shopify.com/s/files/1/0225/4641/5693/files/Review_7_592x.png',
+			srcSet: '//cdn.shopify.com/s/files/1/0225/4641/5693/files/Review_7_530x.png',
 			productHandle: 'miracle-elixir-hair-oil-treatment',
-			productTitle: 'Miracle Hair Elixir',
+			productTitle: '',
 			content: 'This hair oil made my hair VERY smooth, shiny and hydrated after using it 🥰 The elixir is very lightweight, no matter how much or how little you decide to use. I\'m obsessed!',
-			name: '@Nadia'
+			name: ''
+		},
+		{
+			img: '',
+			srcSet: '',
+			productHandle: '',
+			productTitle: '',
+			content: '',
+			name: ''
+		},
+		{
+			img: '',
+			srcSet: '',
+			productHandle: '',
+			productTitle: '',
+			content: '',
+			name: ''
+		},
+		{
+			img: '',
+			srcSet: '',
+			productHandle: '',
+			productTitle: '',
+			content: '',
+			name: ''
+		},
+		{
+			img: '',
+			srcSet: '',
+			productHandle: '',
+			productTitle: '',
+			content: '',
+			name: ''
+		},
+		{
+			img: '',
+			srcSet: '',
+			productHandle: '',
+			productTitle: '',
+			content: '',
+			name: ''
+		},
+		{
+			img: '',
+			srcSet: '',
+			productHandle: '',
+			productTitle: '',
+			content: '',
+			name: ''
+		},
+		{
+			img: '',
+			srcSet: '',
+			productHandle: '',
+			productTitle: '',
+			content: '',
+			name: ''
+		},
+		{
+			img: '',
+			srcSet: '',
+			productHandle: '',
+			productTitle: '',
+			content: '',
+			name: ''
 		}
 	]
 };
