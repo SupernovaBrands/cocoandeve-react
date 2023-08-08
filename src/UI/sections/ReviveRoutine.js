4cb2b215f4649b3339fe7e89bdaf292ddbe12951
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2023-08-08 21:30:11 +0700

    update accordion section

--- src/UI/sections/ReviveRoutine.js
+++ src/UI/sections/ReviveRoutine.js
@@ -4,14 +4,15 @@ const ReviveRoutine = () => {
 			<div className="row align-items-center">
 				<div className="col-12 col-lg-5">
 					<h2 className="youth-revive__routine-title mb-g mb-lg-2">Tangled up in choices? We have you covered.</h2>
-					<p className="youth-revive__routine-sub mb-4 m-lg-0">Like a Virgin vs Youth Revive</p>
+					<p className="youth-revive__routine-sub mb-3 m-lg-0">Like a Virgin vs Youth Revive</p>
 				</div>
-				<div className="col-12 col-lg-7">
+				<div className="col-12 col-lg-7 pt-25 pt-lg-0">
 					<div className="row">
-						<figure className="col-6">
+						<figure className="col-6 pe-0 pe-lg-g youth-revive__routine-img-left mb-0">
 							<a href={`https://www.cocoandeve.com/products/super-nourishing-coconut-fig-hair-masque`}>
 								<picture className="position-relative d-block">
-									<img src="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/1bf377f4-270e-44cd-35ba-93142d8aa100/1140x" className="w-100" />
+									<source type="image/jpeg" media="(min-width: 992px)" srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/1bf377f4-270e-44cd-35ba-93142d8aa100/1140x" width="320" height="360" />
+									<img src="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/1bf377f4-270e-44cd-35ba-93142d8aa100/1140x" width="173" height="173" className="w-100" />
 									<p className="text-white youth-revive__routine-text mb-0">Like A Virgin</p>
 								</picture>
 							</a>
@@ -21,17 +22,18 @@ const ReviveRoutine = () => {
 									<p>Hydrating</p>
 								</div>
 								<strong className="youth-revive__routine-hiw d-block">How it works:</strong>
-								<ul>
+								<ul className="mb-0">
 									<li>Treats dry & damaged hair</li>
 									<li>Improves hair texture and shine</li>
 									<li>Tames frizz and flyaways</li>
 								</ul>
 							</figcaption>
 						</figure>
-						<figure className="col-6">
+						<figure className="col-6 ps-0 ps-lg-g youth-revive__routine-img-right mb-0">
 							<a href={`https://www.cocoandeve.com/products/honey-bliss-hair-set`}>
 								<picture className="position-relative d-block">
-									<img src="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/75e25e00-9f25-473c-d2f3-2ac9a2028800/1140x" className="w-100" />
+									<source srcSet="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/75e25e00-9f25-473c-d2f3-2ac9a2028800/1140x" media="(min-width: 992px)" width="320" height="360" />
+									<img width="173" height="173" src="https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/75e25e00-9f25-473c-d2f3-2ac9a2028800/1140x" className="w-100" />
 									<p className="text-white youth-revive__routine-text mb-0">Youth Revive</p>
 								</picture>
 							</a>
@@ -41,7 +43,7 @@ const ReviveRoutine = () => {
 									<p>Nourishing + Pro Youth<br />+ Scalp care</p>
 								</div>
 								<strong className="youth-revive__routine-hiw d-block">How it works:</strong>
-								<ul>
+								<ul className="mb-0">
 									<li>Strengthens weak, fragile, ageing hair</li>
 									<li>Increase volume and shine</li>
 									<li>Smoothes and softens hair</li>
