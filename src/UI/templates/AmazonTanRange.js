b497a0391e3b876e5f6be3f038a8e70f57f7e787
Teddy Maulana <maulanateddy99@gmail.com>
2023-08-08 10:23:02 +0700

    update banner

--- src/UI/templates/AmazonTanRange.js
+++ src/UI/templates/AmazonTanRange.js
@@ -21,8 +21,8 @@ const AmazonHairRange = () => {
 				jpegBanner: 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/698f76dc-970f-47a9-c99d-f9ab4a839c00/2280x'
 			},
 			us: {
-				webpBanner: 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/451501eb-dde3-41ec-66f6-1d0629e07e00/2280x',
-				jpegBanner: 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/c0a8243b-7e5d-4398-f5d1-088a57479000/2280x'
+				webpBanner: 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/cb0ba0c1-833a-4bdf-9348-0d21d8e49200/1140x',
+				jpegBanner: 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/cb0ba0c1-833a-4bdf-9348-0d21d8e49200/1140x'
 			}
 		},
 		mobile: {
@@ -35,8 +35,8 @@ const AmazonHairRange = () => {
 				jpegBanner: 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/b93abb92-6a08-4555-e5cd-43fd63c22400/828x'
 			},
 			us: {
-				webpBanner: 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/451501eb-dde3-41ec-66f6-1d0629e07e00/2280x',
-				jpegBanner: 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/c0a8243b-7e5d-4398-f5d1-088a57479000/2280x'
+				webpBanner: 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/e223860d-3bf1-45fe-c25e-127092be3800/1140x',
+				jpegBanner: 'https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/e223860d-3bf1-45fe-c25e-127092be3800/1140x'
 			}
 		},
 	};
