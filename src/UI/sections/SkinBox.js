b6e7df5591e8f6b819013e4315698d96673bf2d7
Awais Gill <awais@supernovabrands.com>
2023-09-28 10:46:47 +0500

    skincare update image

--- src/UI/sections/SkinBox.js
+++ src/UI/sections/SkinBox.js
@@ -5,10 +5,10 @@ const ReviveBannerReverse = () => {
 		<section className="youth-revive__section skincare__banner-text-reverse-wrapper skincare__banner pb-4 px-g">
 			<p className="youth-revive__banner-text-reverse-title fw-bold d-lg-block mb-3 youth-revive__reviews mt-3">So, what activates your glow?</p>
             <picture className="d-block w-100">
-                <source srcSet='https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/17ae7941-db70-4c9a-e377-c5e383ed8d00/2280x' media="(min-width: 768px)" type="image/jpeg" />
-                <source srcSet='https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/b417508e-d402-4119-c370-052f97ebf400/828x' type="image/jpeg" />
+                <source srcSet='https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/58c1c530-0b75-4219-04e6-f2deb6dcc200/2280x' media="(min-width: 768px)" type="image/jpeg" />
+                <source srcSet='https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/6b0059ac-01dd-4d56-b8d1-a4eaf2290d00/828x' type="image/jpeg" />
                 <img
-                    src='https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/b417508e-d402-4119-c370-052f97ebf400/828x'
+                    src='https://imagedelivery.net/ghVX8djKS3R8-n0oGeWHEA/6b0059ac-01dd-4d56-b8d1-a4eaf2290d00/828x'
                     alt="Range banner"
                     className="w-100 fit--cover" />
             </picture>
