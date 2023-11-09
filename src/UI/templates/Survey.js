450322fee91fe50ce68e523bf41adbd10a4d296e
Awais Gill <awais@supernovabrands.com>
2023-11-09 10:13:55 +0500

    task/865ct00aa

--- src/UI/templates/Survey.js
+++ src/UI/templates/Survey.js
@@ -400,14 +400,14 @@ const Survey = () => {
         gettingResult(true);
     }
 
-    const skipForm = () => {
-        const stores = ['dev.cocoandeve.com', 'ca.cocoandeve.com', 'de.cocoandeve.com', 'eu.cocoandeve.com', 'uk.cocoandeve.com', 'us.cocoandeve.com', 'www.cocoandeve.com', 'fr.cocoandeve.com']
-        if (stores.includes(selectedSite)) {
-            setSubmitted(true);
-        } else {
-            viewMyResult();
-        }
-    }
+    // const skipForm = () => {
+    //     const stores = ['dev.cocoandeve.com', 'ca.cocoandeve.com', 'de.cocoandeve.com', 'eu.cocoandeve.com', 'uk.cocoandeve.com', 'us.cocoandeve.com', 'www.cocoandeve.com', 'fr.cocoandeve.com']
+    //     if (stores.includes(selectedSite)) {
+    //         setSubmitted(true);
+    //     } else {
+    //         viewMyResult();
+    //     }
+    // }
 
     const postIframeHeight = (key, val) => {
         if (window.top === window.self) return;
@@ -517,7 +517,7 @@ const Survey = () => {
                     }
 
                     { currentPosition === 'finished' && !submitted && additionalStep && !redirect && (
-                        <EmailForm lang={lang} onSubmit={onSubmit} viewMyResult={skipForm} abTest={abTest} />
+                        <EmailForm lang={lang} onSubmit={onSubmit} viewMyResult={viewMyResult} abTest={abTest} />
                     )
                     }
 
